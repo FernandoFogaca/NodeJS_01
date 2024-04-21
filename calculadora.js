c7798cced57b2f3877d37d3dcf16ce7219f9d268
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-04-21 10:17:09 +0200

    Final commit

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,34 @@
 
 
+
+// Função de soma de dois números
+
+
 function somaDoisNumeros( numeroUm, numeroDois ) {
-
-
   return numeroUm + numeroDois;
-    
-    }
+}
 
-     module.exports = { somaDoisNumeros }
-     
 
-     
+  function subtrairDoisNumeros( numeroUm, numeroDois ) { 
     
+    const resultado = numeroUm - numeroDois;
+    return numeroUm - numeroDois;
+
+
+    
+  } 
+
+  function multiplicarDoisNumeros( numeroUm, numeroDois ) {
+    return numeroUm * numeroDois;
+
+  }
+
+  function dividirDoisNumeros( numeroUm, numeroDois ) { 
+
+    return numeroUm / numeroDois;
+  }
+
+
+      
+module.exports = { somaDoisNumeros , subtrairDoisNumeros , multiplicarDoisNumeros, dividirDoisNumeros};
+     
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,30 +1,38 @@
+// Teste unitário para a função somaDoisNumeros
 
-
-const { somaDoisNumeros } = require('./calculadora.js')
+const { somaDoisNumeros , subtrairDoisNumeros, multiplicarDoisNumeros , dividirDoisNumeros     } = require('./calculadora.js')
 
 test("Deve somar dois números corretamente enviando", function() {
-
-
 // Given - Dado que eu tenho tal cenário
 
  const numeroUm = 2;
  const numeroDois = 3;
-
-
 // When - Quando eu executar tal ação
     
      const resultado = somaDoisNumeros(numeroUm, numeroDois);
-
-
 // Then - Então eu espero que aconteça tal coisa
-
 expect(resultado).toBe(5);
 
-
-
-
-
-
-
-
 });
+
+test("Deve subtrair dois números corretamente", function() {
+     const numeroUm = 40;
+     const numeroDois = 20;
+     const resultado = subtrairDoisNumeros(numeroUm, numeroDois); // Corrigido para chamar subtrairDoisNumeros
+     expect(resultado).toBe(20);
+   });
+
+
+test("Deve multiplicar dois números corretamente", function() {
+     const numeroUm = 10;
+     const numeroDois = 10;
+     const resultado = multiplicarDoisNumeros(numeroUm, numeroDois);
+     expect(resultado).toBe(100);
+   });
+
+test("Deve dividir dois números corretamente", function() {
+     const numeroUm = 100;
+     const numeroDois = 10;
+     const resultado = dividirDoisNumeros(numeroUm, numeroDois);
+     expect(resultado).toBe(10);
+   });
